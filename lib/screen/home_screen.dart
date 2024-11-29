d6562bbd5c50f9b9401caec429567be5d892c83b
hai <nguyenhai281103@gmail.com>
2024-11-30 00:56:19 +0700

    2

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -29,7 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Quản lý Chi Tiêu'),
+        title: const Text('Quản lý Chi '),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
